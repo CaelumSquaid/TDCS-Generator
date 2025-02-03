fe1c6977e1d5c00ecbedca263345e8a10af7f3b9
CaelumSquaid <questerkreg.dev@gmail.com>
2025-02-04 00:23:15 +0800

    installed working version for pdfjs-dist v 4.8.69

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@
 import { pdfjs } from "react-pdf";
 import { useState } from "react";
 import { Document, Page } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
 export default function Home() {
+  const [file, setFile] = useState<File>();
   // configure pdfjs
   pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -12,14 +15,24 @@ export default function Home() {
   ).toString();
   return (
     <main className="h-screen w-screen bg-gray-200 flex justify-center items-center">
-      <div className="bg-white rounded border border-gray-300 p-6 shadow-lg">
-        <div>
+      <div className="bg-white rounded border border-gray-300 p-6 shadow-lg h-5/6 flex flex-col gap-4">
+        <div className="flex items-center justify-between pb-2 border border-gray-200 rounded p-2">
           <h1 className="font-semibold">TCDS PDF Generator</h1>
-          <input type="text" />
+          <input
+            type="file"
+            className="text-sm"
+            onChange={(ev) => {
+              ev.currentTarget.files
+                ? setFile(ev.currentTarget.files[0])
+                : null;
+            }}
+          />
         </div>
-        <Document>
-          <Page></Page>
-        </Document>
+        <div className="overflow-y-scroll h-full border border-gray-300 rounded">
+          <Document file={file}>
+            <Page pageNumber={1}></Page>
+          </Document>
+        </div>
       </div>
     </main>
   );
